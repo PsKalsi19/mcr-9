c72e495cb9849abc70828f09c9e2aa90c6975706
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2023-07-29 11:07:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='font-extrabold text-gray-900 text-5xl text-center'>Vite + React</h1>
+
+    </>
+  )
+}
+
+export default App
